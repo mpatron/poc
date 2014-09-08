4de274c61aa6c9db3e3350986f53753d9a0a42ce
Mickaël Patron <mickael_patron@hotmail.com>
2014-09-08 06:55:46 +0200

    WebServiceavec maven

--- WebServiceWithMaven/ExempleWSServer/goo.bat
+++
@@ -1,7 +0,0 @@
-cd D:\ProjetsJava\WebServiceWithMaven\ExempleWSServer\target\wsdl
-
-@rem wsimport -keep -p org.acme.foo.hello -b common.jaxb HelloworldWSService.wsdl
-@rem wsimport -keep -p org.acme.foo.add -b common.jaxb AdditionWSService.wsdl
-
-wsimport -keep -p org.acme.foo.hello HelloworldWSService.wsdl
-wsimport -keep -p org.acme.foo.add AdditionWSService.wsdl
